9e1cbfe0e2191adf38d5be3701ba11422ac76aa6
yshmn-ph <ronnakorn.pho@student.mahidol.edu>
2025-11-18 03:32:15 +0000

    18/11

--- stack.h
+++ stack.h
@@ -16,29 +16,44 @@ public:
 void Stack::push(int x){
   NodePtr new_node=new NODE(x);
   if(new_node){
-            // Left missing for exercises…
+            new_node -> set_next(top);
+            top = new_node;
+            size ++;
    }
+   else cout << "Not enough" << endl;
  
-         // Left missing for exercises…
+         
     
 }
 
 int Stack::pop(){
         NodePtr t=top;
+        if (t!=NULL) {
         int value;
         value=t->get_value();
-    // Left missing part for exercises
+    top = top -> get_next();
+    --size;
         delete t;
         return value;
-	//be careful of the empty stack!!!
+        }
+	cout << "Empty Stack" << endl;
+    return NULL;
+    
     }
 
 Stack::Stack(){
-    //initialize stack
+    top = NULL;
+    size = 0;
     
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
+    cout << "Clear the Stack" << endl;
+    int n = size; 
+    while (n>0) {
+        pop();
+        n--;
+    }
     
 }
 
